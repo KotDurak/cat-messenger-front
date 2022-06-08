b94e77b04ae158a3f0fd30f616cc5739be1ae9c2
maskim <DarkProkuror@mail.ru>
2022-06-08 21:45:57 +0400

    init prototype

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MessagesContent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "MessagesContent"
+    }
+</script>
+
+<style scoped>
+
+</style>
